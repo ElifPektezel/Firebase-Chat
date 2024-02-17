0fd0b299a826cd8a60dab2b8ff17be92d26798f1
Elif Pektezel <140959618+ElifPektezel@users.noreply.github.com>
2024-02-17 21:09:56 +0300

    chat list header

--- app/_layout.js
+++ app/_layout.js
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import { Slot, useRouter, useSegments} from "expo-router";
 import "../global.css";
 import { AuthContextProvider, useAuth } from "../context/authContext";
+import { MenuProvider } from 'react-native-popup-menu';
 
 const MainLayout = () => {
   const { isAuthenticated } = useAuth();
@@ -23,8 +24,11 @@ router.replace('home');
 
 export default function _layout() {
   return (
-    <AuthContextProvider>
+    <MenuProvider>
+       <AuthContextProvider>
       <MainLayout />
     </AuthContextProvider>
+    </MenuProvider>
+   
   );
 }
